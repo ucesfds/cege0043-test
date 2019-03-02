22760fa99220d730d756aa894bc6b1689cb9a190
Francisco Salgado <ucesfds@ucl.ac.uk>
2019-03-02 12:54:45 -0500

    Corrected testAJAX.js

--- testAJAX.js
+++ testAJAX.js
@@ -7,12 +7,12 @@ function callDivChange() {
 }
 
 function processDivChange() {
-	if (xhr.readyState < 4) // while waiting response from
-		server
+	if (xhr.readyState < 4){// while waiting response from server
 		document.getElementById('div1').innerHTML = "Loading...";
+	}
 	else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
-		if (xhr.status == 200 && xhr.status < 300)
-		// http status between 200 to 299 are all successful
-		document.getElementById('div1').innerHTML = xhr.responseText;
+		if (xhr.status == 200 && xhr.status < 300){ // http status between 200 to 299 are all successful
+			document.getElementById('div1').innerHTML = xhr.responseText;
+		}
 	}
 }
