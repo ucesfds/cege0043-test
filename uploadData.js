6a0b37dc8f3f27f5cd3383230ece19c55ab99010
Francisco Salgado <ucesfds@ucl.ac.uk>
2019-03-03 10:59:44 -0500

    Corrected dataUploaded() function

--- uploadData.js
+++ uploadData.js
@@ -1,4 +1,14 @@
 var client; // the global variable that holds the request
+
+function startDataUpload() {
+	var name = document.getElementById("name").value;
+	var surname = document.getElementById("surname").value;
+	var module = document.getElementById("module").value;
+	var postString = "name="+name +"&surname="+surname+"&module="+module;
+	//alert (postString);
+	processData(postString);
+}
+
 function processData(postString) {
 	client = new XMLHttpRequest();
 	client.open('POST','http://developer.cege.ucl.ac.uk:30250/reflectData',true);
@@ -8,19 +18,9 @@ function processData(postString) {
 }
 // create the code to wait for the response from the data server, and process the response once it is received
 function dataUploaded() {
-	// this function listens out for the server to say that the data is ready - i.e.
-	has state 4
+	// this function listens out for the server to say that the data is ready - i.e. has state 4
 	if (client.readyState == 4) {
 		// change the DIV to show the response
 		document.getElementById("dataUploadResult").innerHTML = client.responseText;
 	}
-}
-
-function startDataUpload() {
-	var name = document.getElementById("name").value;
-	var surname = document.getElementById("surname").value;
-	var module = document.getElementById("module").value;
-	var postString = "name="+name +"&surname="+surname+"&module="+module;
-	//alert (postString);
-	processData(postString);
 }
